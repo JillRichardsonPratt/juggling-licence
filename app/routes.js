2a3ba9ed657ca5dd082f765b1d06376635dd415e
JillRichardsonPratt <jill.richardsonpratt@digital.hmrc.gov.uk>
2019-08-15 12:15:59 +0100

    Add juggling objects journey

--- app/routes.js
+++ app/routes.js
@@ -15,4 +15,18 @@ router.post('/juggling-balls-answer', function (req, res) {
     res.redirect('/ineligible')
   }
 })
+
+router.post('/juggling-objects-answer', function (req, res) {
+
+  //Make a variable and give it the value from juggling-balls
+  var jugglingObjects = req.session.data['juggling-objects']
+console.log(jugglingObjects)
+  //Check whether the variable matches a condition
+  if (jugglingObjects.includes("Fiery sticks")) {
+    //Send user to next page
+    res.redirect('/juggling-danger')
+  } else {
+    res.redirect('/check-your-answers')
+  }
+})
 module.exports = router
